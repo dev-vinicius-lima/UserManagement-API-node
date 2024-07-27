d681a9dc35f444ceb64db18576be68c18e245b27
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-07-26 20:52:58 -0400

    feat: :sparkles: config cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import router from './routes/routes.js';
-
+import cors from 'cors';
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.use('/', router);
 
